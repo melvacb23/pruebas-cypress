d9b86d0a23c3c3ab15217d02d6cfd31107896de3
melvacb23 <melvacb23@gmail.com>
2026-06-16 20:07:09 -0500

    actualizacion

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,24 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'mochawesome',
+  video: true,
+  videosFolder: "cypress/videos",
+
+  reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: true,
-    json: true
+    reportDir: "cypress/reports",
+    charts: true,
+    reportPageTitle: "Reporte de Pruebas Cypress",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    ignoreVideos: false,
+    videoOnFailOnly: false
   },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
-})
+});
